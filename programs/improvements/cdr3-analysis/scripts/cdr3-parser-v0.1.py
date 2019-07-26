b1e37ce4a6429da44bb2cb667f3156d5d6259bcb
Cardosaum <mcardosos99@gmail.com>
2019-07-26 16:26:31 -0300

    added regex for all patterns in VH sequences

--- programs/improvements/cdr3-analysis/scripts/cdr3-parser-v0.1.py
+++ programs/improvements/cdr3-analysis/scripts/cdr3-parser-v0.1.py
@@ -2,15 +2,33 @@ import os
 import time
 import re
 
-with open('/home/matheus/mcs/study/code/bioinfo/attila/programs/improvements/cdr3-analysis/files/R0/test01.txt') as file:
+
+cdr3_VH_Regex = re.compile(r'((C)(\w+)(C)(..)(\w+)(WG.G))')
+
+read = False
+
+hashRegex = re.compile(r'(([\d\w]+)(:)(\d+)(:)(\d+)(-)([\d\w]+)(:)(\d)(:)(\d+)(:)(\d+)(:)(\d+)([|])(FRAME:(\d))([|])(\[(\d+)-(\d+)\])([|])(\d+)([|])(\d+)(\.)(\d+))')
+
+asterixRegex = re.compile(r'((C)(\w+)(C)(.{2})(\w+)(WG.G))')
+
+greaterRegex = re.compile(r'((\w+)(:)(\w+)(:)(\d+)(-)(\w+)(:)(\d)(:)(\d+)(:)(\d+)(:)(\d+)([|])(FRAME:(\d+))(\s+)(\w+))')
+
+seqRegex = re.compile(r'((.+)(C)(.+)(C)(.{2})(.+)(WG.G)(.+))')
+
+with open('/home/matheus/mcs/wo/R0/Renato_zika_acido_R0_VH_R1aafreq.txt', encoding='ISO-8859-1') as file:
     for line in file:
-        if line[0] == '*':
-            print(line)
-            for char in range(len(line)):
-                if line[char] == 'C':
-                    print(line[char], 'at index: ', char)
+      if line[0] == '#':
+          parse = hashRegex.search(line)
+          # TODO: find what is the usefull data here
+      elif line[0] == '*':
+          parse = asterixRegex.search(line)
+      elif line[0] == '>':
+          parse = greaterRegex.search(line)
+          read = True
+      elif read:
+          parse = seqRegex.search(line)
+          print(parse[7])
+          read = False
 
 
-
-                    print('So, CDR3 is: {}'.format(line[char+2]))
 
