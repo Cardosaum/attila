3d06b2f6c34215dc1070ccda69d908e535755610
Cardosaum <mcardosos99@gmail.com>
2019-11-30 17:08:16 -0300

    more modifications

--- programs/convertofasta.pl
+++ programs/convertofasta.pl
@@ -5,9 +5,9 @@ use strict;
 #--------------------------------------------------------------------------------------------------------------
 # Programa:convertofasta.pl
 # Este programa recebe como entrada um arquivo que contem uma lista de sequências dotadas
-# de id e sequência. A sequência se encontra em formato de colunas numeradas, em que cada 
+# de id e sequência. A sequência se encontra em formato de colunas numeradas, em que cada
 # resíduo de aminoácido recebe um número de acordo com o esquema de numeração de imunoglobulinas
-# de Kabat. O programa concatene cada um dos caracteres que representam os aminoácidos para formar 
+# de Kabat. O programa concatena cada um dos caracteres que representam os aminoácidos para formar
 # uma sequência de domínio variável completa e imprime a sequência em formato fasta.
 #--------------------------------------------------------------------------------------------------------------
 
@@ -21,7 +21,7 @@ my %cdrs;
 # Passo. Se o usuário entrar com menos argumentos que o necessário
 if (@ARGV < 2)
 {
-# 	Passo. Imprima mensagem de erro e pare a execução do programa	
+# 	Passo. Imprima mensagem de erro e pare a execução do programa
 	die "convertfasta.pl: perl convertofasta.pl <inputfile> <outputfile> \n";
 }
 my ($input,$output) = @ARGV;
@@ -50,11 +50,11 @@ while ($line = <IN>)
 		$cdrs{$aux1}{seq} = "";
 # 		Passo. Atribua o contador de leitura para o hash number
 		$cdrs{$aux1}{number} = $number;
-# 		Passo. Incremente o contador de leitura	
+# 		Passo. Incremente o contador de leitura
 		$number++;
 	}
 # 	Passo. Senão
-	else 
+	else
 	{
 # 		Passo. Se a linha for de uma coluna de resíduo numerado
 		if ($line =~ /([A-Z]?)$/)
@@ -89,4 +89,4 @@ foreach $line (sort{$cdrs{$a}{number} <=> $cdrs{$b}{number}} keys %cdrs)
 close (OUT);
 
 exit(0);
-		
+
