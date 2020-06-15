2b108df5d12a9b9b8625e0a47e83cdf5e7ccf3c0
Cardosaum <mcardosos99@gmail.com>
2020-06-15 20:25:16 -0300

    moved `subprocess.run` code to execute after the print of the file name

--- programs/cdr/parse_data.py
+++ programs/cdr/parse_data.py
@@ -20,10 +20,9 @@ for i in files_path_raw:
             filename_raw = "_".join(i.parts[3:])
             filename_parsed = pathlib.Path(pathlib.Path(filename_raw).stem + ".csv")
             file_path_parsed = pathlib.Path("./data/parsed/", filename_parsed).resolve()
-            subprocess.run(["/usr/bin/python", str(cdr_parser_path), i.resolve(), file_path_parsed], check=True)
-            time_stop = time.perf_counter() - time_start
             print(filename_raw)
             print(filename_parsed)
+            subprocess.run(["/usr/bin/python", str(cdr_parser_path), i.resolve(), file_path_parsed], check=True)
+            time_stop = time.perf_counter() - time_start
             print(f"Elapsed time: {time_stop}")
-
             print("=" * 30)
