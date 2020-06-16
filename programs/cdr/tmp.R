6dc37c3c501d47967120df3df291905aae03337c
Cardosaum <mcardosos99@gmail.com>
2020-06-16 18:27:35 -0300

    - write a function that load and merge the passed files - add collumn type specification (now the dataframes are loaded faster) - clean the final dataframe - started EDA

--- programs/cdr/tmp.R
+++ programs/cdr/tmp.R
@@ -1,16 +1,55 @@
 library(tidyverse)
+library(magrittr)
 
-files <- dir("./data/parsed/")
+data_path <- "./data/parsed"
+files <- dir(data_path)
 
-all <- vector()
-for (i in seq_along(files)){
-    if (i == 1){
-        print(files[i])
-        tmp <- read_csv(file.path("./data/parsed", files[i]))
-        all[i] <- tmp
-        print(file.path("./data/parsed/", files[i]))
+load_cdr <- function(file_names){
+    df <- NULL
+    for (i in file_names){
+        print(i)
+        if (!is_tibble(df)){
+            df <- read_csv(i, col_types = "ciiddddddddiiiiiiiiiiiiiiiiiiiiiiiiiic")
+        } else {
+            tmp <- read_csv(i, col_types = "ciiddddddddiiiiiiiiiiiiiiiiiiiiiiiiiic")
+            df %<>% full_join(tmp)
+        }
     }
+    df
 }
 
+files <- files[str_detect(files, "(?i)isaura")]
+# match only lines that does not contains "falho" or "teste"
+files <- files[str_detect(files, "^(?:(?!falho|teste).)*$")]
+# merge file name and file path
+files <- sapply(files, function(x){file.path(data_path, x)})
 
-all[1] <- read_csv(file.path("./data/parsed", files[i]))
+cdr_df <- load_cdr(files)
+
+cdr_df %<>% group_by(file)
+
+cdr_df %>% mutate(cdrp = quantity/sum(quantity)) %>%
+            select(cdrp, cdr3, quantity, file) %>% 
+            filter(cdrp == max(cdrp)) -> ham
+
+# write_rds(cdr_df, file.path(data_path, "isaura_compressed.rds"))
+
+# cdr_df <- read_rds(file.path(data_path, "isaura_compressed.rds"))
+
+files_duplicated <- ham$file[duplicated(ham$file)] %>% unique()
+
+ham %>% select(cdrp, cdr3, quantity, file) %>% 
+        filter(file %in% files_duplicated) %>% 
+        filter(cdrp == max(cdrp)) -> bagar
+
+wh <- read_csv(file.path(data_path, "mariajac_isaura_2_2_isaura_H_0eX4g_L_0bX4c_VH_FinalRound_VLR42c_S10_L001_R1_001aafreq.csv"), col_types = "ciiddddddddiiiiiiiiiiiiiiiiiiiiiiiiiic")
+
+cdr_df %>% mutate(cdrp = quantity/sum(quantity)) %>% 
+            slice_head(n = 5) -> cdr
+
+cdr %<>% select(cdr3, cdrp, everything()) %>% 
+            arrange(-cdrp, .by_group = TRUE)
+
+ggplot(cdr) +
+    geom_point(aes(cdrp, MW, color = file)) +
+    scale_color_discrete(labels = NULL)
