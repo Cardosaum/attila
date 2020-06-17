718b6cb76c25eb020a1105e0819c886d86e0c15b
Cardosaum <mcardosos99@gmail.com>
2020-06-17 19:37:07 -0300

    formated file

--- programs/cdr/tmp.R
+++ programs/cdr/tmp.R
@@ -29,7 +29,7 @@ cdr_df <- load_cdr(files)
 cdr_df %<>% group_by(file)
 
 cdr_df %>% mutate(cdrp = quantity/sum(quantity)) %>%
-            select(cdrp, cdr3, quantity, file) %>% 
+            select(cdrp, cdr3, quantity, file) %>%
             filter(cdrp == max(cdrp)) -> ham
 
 # write_rds(cdr_df, file.path(data_path, "isaura_compressed.rds"))
@@ -38,19 +38,19 @@ cdr_df %>% mutate(cdrp = quantity/sum(quantity)) %>%
 
 files_duplicated <- ham$file[duplicated(ham$file)] %>% unique()
 
-ham %>% select(cdrp, cdr3, quantity, file) %>% 
-        filter(file %in% files_duplicated) %>% 
+ham %>% select(cdrp, cdr3, quantity, file) %>%
+        filter(file %in% files_duplicated) %>%
         filter(cdrp == max(cdrp)) -> bagar
 
 wh <- read_csv(file.path(data_path, "mariajac_isaura_2_2_isaura_H_0eX4g_L_0bX4c_VH_FinalRound_VLR42c_S10_L001_R1_001aafreq.csv"), col_types = "ciiddddddddiiiiiiiiiiiiiiiiiiiiiiiiiic")
 
-cdr_df %>% mutate(cdrp = quantity/sum(quantity)) %>% 
+cdr_df %>% mutate(cdrp = quantity/sum(quantity)) %>%
             slice_head(n = 5) -> cdr
 
-cdr %<>% select(cdr3, cdrp, everything()) %>% 
+cdr %<>% select(cdr3, cdrp, everything()) %>%
             arrange(-cdrp, .by_group = TRUE)
 
-cdr %>% slice_head(n = 1) %>% 
+cdr %>% slice_head(n = 1) %>%
 ggplot() +
     geom_point(aes(cdrp, log(quantity), color = file)) +
     scale_color_discrete(labels = NULL)
